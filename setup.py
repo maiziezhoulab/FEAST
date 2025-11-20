2b83e80599946d0db4d9099571659faa0a82a701
chowx <chowx@MAIZIE-GPU2.vuds.vanderbilt.edu>
2025-11-20 02:07:38 -0600

    Rename package to FEAST-py for PyPI publication

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="FEAST-sim",
+    name="FEAST-py",
     version="0.1.7",
     author="Yiru CHEN",
     author_email="yiru.22@intl.zju.edu.cn",
